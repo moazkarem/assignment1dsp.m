fa5a479de37330abbc92e75af3792ed296e542e6
moazkarem <93332298+moazkarem@users.noreply.github.com>
2021-10-29 23:01:05 +0200

    Add files via upload

---
+++ assignment1dsp (3).m
@@ -0,0 +1,31 @@
+﻿A=[-7 -5 -9; 2 -1 2; 1 -1 2];
+B=[16 3 2 13;5 10 11 8; 9 6 7 12; 4 15 14 1];
+C=[4 2 -3; 7 -7 9; 3 -5 6];
+D=[6 3 2; 2 12 -7; -1 6 2; -5 15 11];
+x=3*A-5*c
+s=7*A+2*B
+y=C*A
+m=C*D'
+zeros(5)
+zeros(5,6)
+ones(5)
+ones(5,6)
+size(D)
+zeros(size(D))
+diag([1 2 3 4])
+eye(5)
+[A,b]
+%error becous size(A)=3*3 & size(b)=4*4 
+[A;B]
+% error: 'b' undefined near line 1, column 1
+A(1,:)
+A(,:1)
+z=diag([5 5 5 5 5 5 5]);
+e=[5;5;5;5;5;5;5];
+[z,e]
+
+
+
+
+
+
